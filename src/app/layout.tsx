1f279b4bcd6eba56a18dd16a19b413e5f47e3856
Alred <prottoyrudra7777@gmail.com>
2023-12-11 10:18:40 +0000

    added googel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from 'next'
 
 import { RootLayout } from '@/components/RootLayout'
+import GoogleAnalytics from './GoogleAnalytics';
 
 import '@/styles/tailwind.css'
 
@@ -15,6 +16,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
+        <GoogleAnalytics />
         <RootLayout>{children}</RootLayout>
       </body>
     </html>
